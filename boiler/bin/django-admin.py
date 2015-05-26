5d7fdc9428f204bc3449cb5970c79f0406c25555
Chris Hackett <chackett87@gmail.com>
2015-05-26 11:28:33 -0700

    base shell

---
+++ boiler/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/RandomRain/coding/boiler_plate/django-drf-starter-project/boiler/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
